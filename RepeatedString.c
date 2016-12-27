b4cb287bef147577817c67849fe6193010a586fb
Amey Deshmukh <ameydeshmukh2@gmail.com>
2016-12-27 20:07:27 +0530

    Create RepeatedString.c

---
+++ RepeatedString.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    char* s = (char *)malloc(512000 * sizeof(char));
+    scanf("%s",s);
+    long n; 
+    scanf("%ld",&n);
+    return 0;
+}
